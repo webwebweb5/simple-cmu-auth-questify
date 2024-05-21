f160754404328b4fcb38fc3b8cc9f37dbe84f7aa
webwebweb5 <phiriyakornman@gmail.com>
2024-05-22 01:02:40 +0700

    Add suspense

--- app/callback/page.jsx
+++ app/callback/page.jsx
@@ -2,7 +2,7 @@
 
 import { AuthContext } from "@/auth/context/auth-context";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { Suspense, useContext, useEffect } from "react";
 
 export default function Page() {
   const { login, user } = useContext(AuthContext);
@@ -20,13 +20,13 @@ export default function Page() {
 
   useEffect(() => {
     if (user) {
-        router.push("/profile")
+      router.push("/profile");
     }
   }, [router, user]);
 
   return (
-    <main className="flex w-[100vw] h-[100vh] items-center justify-center">
-      <div>Loading...</div>
-    </main>
+    <Suspense fallback={<div>loading...</div>}>
+      <div>loading...</div>
+    </Suspense>
   );
 }
